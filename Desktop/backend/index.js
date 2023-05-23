6395d7df65af0fe7050bfb360afa5022512b83a9
Gustamesquitta <gustavofreitas63@hotmail.com>
2023-05-23 20:01:08 -0300

    Atualização back carrinho

--- Desktop/backend/index.js
+++ Desktop/backend/index.js
@@ -22,6 +22,13 @@ server.post('/carrinho', function(req,res){
   server.get('/carrinho', function (req,res){
     return res.status(200).json(carrinho)
   })
+
+//  server.delete('/carrinho/id', function (req,res){
+//     const produtoId = req.params.id
+//     carrinho.delete(produtoId)
+//     return res.status(200).json(carrinho)
+//   })
+
 server.listen(3000, () =>{
  console.log('Servidor esta funcionando!');
 });
--- Desktop/backend/src/data/carrinho.js
+++ Desktop/backend/src/data/carrinho.js
@@ -4,18 +4,24 @@ const app = express();
 let carrinho = [];
 
 app.get('/carrinho', (req, res) => {
-  const item = req.query.item;
-  console.log(`Adicionando item ao carrinho: ${item}`);
-
-  carrinho.push(item);
-
-  res.json({ message: `Item adicionado ao carrinho: ${item}` });
+  res.status(200).json(carrinho);
 });
-app.post('/carrinho', function(req,res){
-  const produto = req.body
-  carrinho.push(produto) 
-  return res.status(200).json(carrinho)
-})
+
+app.post('/carrinho', (req, res) => {
+  const produto = req.body;
+  carrinho.push(produto);
+  res.status(200).json(carrinho);
+});
+
+// app.delete('/carrinho/:id/:id', (req, res) =>{
+//   const produtoId = req.params.id;
+//   const produtoIndex = carrinho.findIndex(produto => produto.id === produtoId);
+//   if (produtoIndex !== -1 ){
+//     carrinho.splice(produtoIndex, 1);
+//     res.status(200).json({mensage:'O produto foi removido'});
+//   }
+//   else {res.status(404).json({mensage:'Produto não encontrado'});}})
+
 app.listen(3000, () => {
   console.log('API está em execução em http://localhost:3000');
 });
